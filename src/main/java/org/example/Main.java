31394037df5e9bf37bf46f46e94ad72d37dd95ab
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-02-08 16:24:24 +0545

    Integrated RealTimeUpdater in Main to start real-time stock updates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,10 @@
 package org.example;
 
 
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         StockMarket stockMarket = new StockMarket();
-        List<StockPrice> stockPrices = stockMarket.getAllStockPrices();
-
-
-        stockPrices.forEach(System.out::println);
-
+        RealTimeUpdater realTimeUpdater = new RealTimeUpdater(stockMarket);
+        realTimeUpdater.startRealTimeUpdates();
     }
 }
